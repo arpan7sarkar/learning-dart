6969e3d0b23009a7b3723f74f6c3b12abc346d33
arpan7sarkar <arpansakar@gmail.com>
2025-09-04 18:34:11 +0530

    list basics done

---
+++ dart basics/4_lists.dart
@@ -0,0 +1,19 @@
+//list is similer to  array or i would say python ke chiz😂+ js ke jesa mixeddatatype support
+void main() {
+  var myList=["hello",5,5.8,true,0,null,"33",0,"done"];
+  var yourList=["ok","here",55,0.1,22,4,false];
+  print(myList[1]);
+  print(myList.sublist(3,8));//it also has sublist similer to java
+  //lets join two lists
+  print(myList+yourList);//OMG its that easy☠️
+  print(myList.sublist(0,3)+yourList.sublist(3,5));//to much amazing
+
+  //it also copied spread operator from js
+  print([4,2,5,...myList]);//🌪️🌪️
+
+  //one corn is can't use negative indexing
+  // print(myList[-1]); wrong syntax🥲
+
+  
+}
+//dart===mix of many programing languages in a single pack ithink 🤓😂

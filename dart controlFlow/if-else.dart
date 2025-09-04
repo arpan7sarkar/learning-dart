b8ef2b8f82077760bb8e8ad1a91b932e3f222f66
arpan7sarkar <arpansakar@gmail.com>
2025-09-04 19:44:16 +0530

    if else for for each loop

---
+++ dart controlFlow/if-else.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  /**
+   if (condition) {
+    // code to be executed if condition is true
+  }
+  else if (condition) {
+    // code to be executed if condition is true
+  }
+  else {
+    // code to be executed if condition is false
+  }
+   */
+
+}
---
+++ dart controlFlow/loops.dart
@@ -0,0 +1,10 @@
+//for loop is same but for each loop is different
+void main(List<String> args) {
+  for (var i = 0; i < 10; i++) {
+    print(i);
+  }
+  var nums=[1,2,3,4,5];
+  for(final num in nums){
+    print( num);
+  }
+}

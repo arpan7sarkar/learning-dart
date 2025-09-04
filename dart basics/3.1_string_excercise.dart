5b6e40af0430e7daeedcae9b6294879b0a570cb2
arpan7sarkar <arpansakar@gmail.com>
2025-09-04 18:18:08 +0530

    done

---
+++ dart basics/3.1_string_excercise.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Given the string below, replace all the
+  // "x"s with a blank string
+  var errorMessage = "Uxh oxh, thexre's a bunxch of txypos";
+  errorMessage=errorMessage.replaceAll("x", "");
+  print(errorMessage);
+
+}

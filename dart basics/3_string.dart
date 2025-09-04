d0e0e41deea6348aca8c36e7ab42c93723e28731
arpan7sarkar <arpansakar@gmail.com>
2025-09-04 18:14:29 +0530

    basic var operations in stirng done

---
+++ dart basics/3_string.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name1 = "Arpan";
+  print(name1.toUpperCase());
+  var value = 11;
+  print("The value is $value");//to acces any value in a string we need to use $ sign
+  print("TO use \$ ");//using $ sign in any sting 
+  //for more js suitability we can write 
+  print("Here i am doing an arithmetic operation ${value+8-1*2}");//almost like js + python😂😂
+  print("The uppercase of $name1 is ${name1.toUpperCase()}");
+}

739a877be3de88231f69f50bea9e0f52ee3590e4
arpan7sarkar <arpansakar@gmail.com>
2025-09-04 17:37:37 +0530

    played with basic variables in dart

---
+++ variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  //so here i just get to know that dart follows similer format of declering variable as old js
+  var num1=6,num2=5;
+  var sum=num1+num2;
+  var name="I am Arpan";
+  var s="omg";
+  print(s.toUpperCase());
+  print("what is my name ans: "+name);
+  print(sum);
+
+}
